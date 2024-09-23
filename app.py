d04ea2ff07ce7597d823998f9e2f1f0b3c517ae5
Yuhan Zhang <yuhanzhang@sherley-2.local>
2024-09-22 21:44:26 -0400

    excution for sqrtroot

--- app.py
+++ app.py
@@ -37,3 +37,17 @@ print("3, multiply")
 print("4, divide")
 print("5, power")
 print("6, square root")
+
+#execution
+
+while True:
+    choice=input("Please Enter Operation Choice: ")
+
+    if choice == "6":
+        try: 
+            num=float(input("Please enter number that you wish to do the square root: ")
+        except ValueError:
+            print("Invalid Input, enter number only")
+            continue
+        
+        print(f"square root of {num} = {sqrtroot(num)}")
